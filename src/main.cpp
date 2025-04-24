4dfcb5c85e568980a7703df6b6faab08012eb4e8
Corbin <63320116+Tw1ZZLER@users.noreply.github.com>
2025-04-24 13:29:03 -0400

    New Stuff :)

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,44 @@
 #include <Arduino.h>
+#include <BMI088.h>
+#include <Wire.h>
+#include <stdio.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// #define I2C_SCL 0
+// #define I2C_SDA 0 
+// #define I2C_PORT_NUMBER 0
+#define I2C_ADDRESS_ACCEL_1 0x18
+#define I2C_ADDRESS_GYRO_1 0x68
+
+Bmi088 bmi(Wire,I2C_ADDRESS_ACCEL_1,I2C_ADDRESS_GYRO_1);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+
+  printf("Hello BMI088\n");
+
+  // Accelerometer Setup
+  bmi.begin();
+  bmi.setOdr(Bmi088::ODR_1000HZ);
+  bmi.setRange(Bmi088::ACCEL_RANGE_6G,Bmi088::GYRO_RANGE_500DPS);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  // Grab Accelerometer Values  
+  bmi.readSensor();
+  float t, ax, ay, az, gx, gy, gz;
+  t = bmi.getTemperature_C();
+  ax = bmi.getAccelX_mss();
+  ay = bmi.getAccelY_mss();
+  az = bmi.getAccelZ_mss();
+  gx = bmi.getGyroX_rads();
+  gy = bmi.getGyroY_rads();
+  gz = bmi.getGyroZ_rads();
+  uint64_t time_ps;
+  time_ps = bmi.getTime_ps();
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  printf("Time: %d Temp: %d°C Gyro: %f %f %f Accel: %f %f %f\n",
+          time_ps, t,           gx, gy, gz,     ax, ay, az);
+  // printf("System Status: %d", status);
+
+  delay(5);
 }
