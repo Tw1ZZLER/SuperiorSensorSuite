8e1516dfa508a249def6faab05f79de78a3871de
c-dusek <c-dusek@onu.edu>
2025-05-06 11:46:21 -0400

    angles are like half-working

--- src/main.cpp
+++ src/main.cpp
@@ -10,47 +10,84 @@
 Bmi088 bmi(Wire,I2C_ADDRESS_ACCEL_1,I2C_ADDRESS_GYRO_1);
 
 int status = 0;
+float xDisplace, yDisplace, zDisplace;
+unsigned long lastTime = 0;
 
 void setup() {
   Serial.begin(115200);
 
-  Serial.printf("Hello BMI088\n");
+  Serial.println("Hello BMI088");
 
   // Accelerometer Setup
   status = bmi.begin();
   bmi.setOdr(Bmi088::ODR_1000HZ);
   bmi.setRange(Bmi088::ACCEL_RANGE_6G,Bmi088::GYRO_RANGE_500DPS);
+  lastTime = millis();
+}
+
+// hopefully does what i want it to
+float angDisplace(float angularVelocity, float time) {
+  if(angularVelocity == 0)
+    return 0;
+  return angularVelocity * (time/1000);
 }
 
 void loop() {
+
+  unsigned long now = millis();
+  float deltaTime = (now - lastTime);
+  lastTime = now; 
+
   // Grab Accelerometer Values  
   bmi.readSensor();
+  
   char buf[100] = {};
-  float t, ax, ay, az, gx, gy, gz;
+  float t, ax, ay, az, gx, gy, gz, xChange = 0, yChange = 0, zChange = 0, time;
+
   t = bmi.getTemperature_C();
   ax = bmi.getAccelX_mss();
   ay = bmi.getAccelY_mss();
   az = bmi.getAccelZ_mss();
-  gx = (bmi.getGyroX_rads() / PI) * 180;
-  gy = (bmi.getGyroY_rads() / PI) * 180;
-  gz = (bmi.getGyroY_rads() / PI) * 180;
 
-  Serial.print("Temp: ");
-  Serial.print(t, 4);
-  Serial.print("°C Gyro: ");
-  Serial.print(gx, 4);
-  Serial.print(" ");
-  Serial.print(gy, 4);
-  Serial.print(" ");
-  Serial.print(gz, 4);
-  Serial.print(" Accel: ");
-  Serial.print(ax, 4);
-  Serial.print(" ");
-  Serial.print(ay, 4);
-  Serial.print(" ");
-  Serial.print(az, 4);
-  Serial.print(" Status: ");
-  Serial.println(status);
+  gx = bmi.getGyroX_rads();
+  gy = bmi.getGyroY_rads();
+  gz = bmi.getGyroZ_rads();
 
-  delay(500);
+  xChange = angDisplace(gx, deltaTime);
+  yChange = angDisplace(gy, deltaTime);
+  zChange = angDisplace(gz, deltaTime);
+
+  xDisplace += xChange;
+  yDisplace += yChange;
+  zDisplace += zChange;
+
+  float xDisplay = (xDisplace / PI) * 180;
+  float yDisplay = (yDisplace / PI) * 180;
+  float zDisplay = (zDisplace / PI) * 180;
+
+  // Serial.print("Temp: ");
+  // Serial.print(t, 4);
+  // Serial.print("°C Gyro: ");
+  // Serial.print(gx, 4);
+  // Serial.print(" ");
+  // Serial.print(gy, 4);
+  // Serial.print(" ");
+  // Serial.print(gz, 4);
+  // Serial.print(" Accel: ");
+  // Serial.print(ax, 4);
+  // Serial.print(" ");
+  // Serial.print(ay, 4);
+  // Serial.print(" ");
+  // Serial.print(az, 4);
+  // Serial.print(" Status: ");
+  // Serial.println(status);
+  Serial.print("X: ");
+  Serial.print(xDisplay, 4);
+  Serial.print(" Y: ");
+  Serial.print(yDisplay, 4);
+  Serial.print(" Z: ");
+  Serial.println(zDisplay, 4);
+
+  //delay(20);
 }
+
